6982da6ae9e16eed3bc5725f4ce54bf7bb639c1e
Azzam Makki <azzam@smartretail.co>
2024-05-12 18:15:54 +0800

    Move login form into a sperate component as Login.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import logo from './assets/images/bootstrap-logo.svg';
+import Login from './pages/Login';
 
 function App() {
   return (
-    <>
-      <main className="form-user w-100 m-auto">
-        <form>
-          <img className="mb-4" src={logo} alt="" width="72" height="57"/>
-          <h1 className="h3 mb-3 fw-normal">Please login</h1>
-
-          <div className="form-floating">
-            <input type="email" className="form-control" id="floatingInput" placeholder="name@example.com"/>
-            <label for="floatingInput">Email address</label>
-          </div>
-          <div className="form-floating">
-            <input type="password" className="form-control" id="floatingPassword" placeholder="Password"/>
-            <label for="floatingPassword">Password</label>
-          </div>
-
-          <div className="border-top pt-3 mb-3">
-              <small className="text-muted">
-                  Need An Account? <a className="ml-2" href="register.html">Register Now</a>
-              </small>
-          </div>
-
-          <button className="btn btn-primary w-100 py-2" type="submit">Login</button>
-        </form>
-      </main>
-    </>
+    <Login />
   );
 }
 
