8b4f88770ed6dbfc953c4a340157e47768208ac5
Azzam Makki <azzam@smartretail.co>
2024-05-15 22:33:33 +0800

    Apply sidebar with react router dom and pages routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import Login from './pages/Login';
 import Home from './pages/Home.js';
+import Tasks from './pages/Tasks.js';
+import CompletedTasks from './pages/CompletedTasks.js';
+import PendingTasks from './pages/PendingTasks.js';
 import Header from './layout/Header';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Header/>
-      {/* <Router>
-        <Routes>
-          <Route exact path="/" element={<Home />}/>
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </Router> */}
+        <Router>
+          <div className="container-fluid">
+            <div className="row">
+              <Header/>
+              <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <Routes>
+                  <Route exact path="/" element={<Home />}/>
+                  <Route exact path="/tasks" element={<Tasks />}/>
+                  <Route exact path="/completed-tasks" element={<CompletedTasks />}/>
+                  <Route exact path="/pending-tasks" element={<PendingTasks />}/>
+                  <Route path="/login" element={<Login />} />
+                </Routes>
+              </main>
+            </div>
+          </div>
+        </Router>
     </>
   );
 }
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
@@ -41,8 +42,8 @@ const Header = () => {
             </ul>
         </header>
 
-        <div className="container-fluid">
-            <div className="row">
+        {/* <div className="container-fluid">
+            <div className="row"> */}
 
                 <div className="sidebar border border-right col-md-3 col-lg-2 p-0 bg-body-tertiary  ">
                     <div className="offcanvas-md offcanvas-end bg-body-tertiary" tabIndex="-1" id="sidebarMenu" aria-labelledby="sidebarMenuLabel">
@@ -53,28 +54,28 @@ const Header = () => {
                         <div className="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
                             <ul className="nav flex-column">
                                 <li className="nav-item">
-                                <a className="nav-link d-flex align-items-center gap-2 active" aria-current="page" href="#">
+                                <Link className="nav-link d-flex align-items-center gap-2 active" aria-current="page" to="/">
                                     <svg className="bi"><use xlinkHref="#house"/></svg>
                                     Dashboard
-                                </a>
+                                </Link>
                                 </li>
                                 <li className="nav-item">
-                                <a className="nav-link d-flex align-items-center gap-2" href="tasks.html">
+                                <Link className="nav-link d-flex align-items-center gap-2" to="/tasks">
                                     <svg className="bi"><use xlinkHref="#file-earmark"/></svg>
                                     Tasks
-                                </a>
+                                </Link>
                                 </li>
                                 <li className="nav-item">
-                                <a className="nav-link d-flex align-items-center gap-2" href="pending_tasks.html">
+                                <Link className="nav-link d-flex align-items-center gap-2" to="/pending-tasks">
                                     <svg className="bi"><use xlinkHref="#file-earmark-text"/></svg>
                                     Pending Tasks
-                                </a>
+                                </Link>
                                 </li>
                                 <li className="nav-item">
-                                <a className="nav-link d-flex align-items-center gap-2" href="completed_tasks.html">
+                                <Link className="nav-link d-flex align-items-center gap-2" to="/completed-tasks">
                                     <svg className="bi"><use xlinkHref="#check-circle"/></svg>
                                     Completed Tasks
-                                </a>
+                                </Link>
                                 </li>
                             </ul>
 
@@ -92,14 +93,8 @@ const Header = () => {
                     </div>
                 </div>
 
-                <main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                    <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                        <h1 className="h2">Dashboard</h1>
-                    </div>
-                </main>
-
-            </div>
-        </div>
+            {/* </div>
+        </div> */}
     </>
     )
 }
