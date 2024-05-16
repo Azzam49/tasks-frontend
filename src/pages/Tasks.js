c3ea166a9acbb3bc252efcabc70ff9a0af6b2ca7
Azzam Makki <azzam@smartretail.co>
2024-05-16 18:31:52 +0800

    On tasks table, set the correct badge color by conditon check for status column

--- src/pages/Tasks.js
+++ src/pages/Tasks.js
@@ -53,7 +53,17 @@ const Tasks = () => {
                             <td>{task.title}</td>
                             <td><span className="badge text-bg-secondary">{task.tag}</span></td>
                             <td>{task.created_at}</td>
-                            <td><span className="badge text-bg-success">{task.status}</span></td>
+                            <td>
+                                {/* {
+                                    task.status === 'Completed' ?
+                                        <span className="badge text-bg-success">{task.status}</span>
+                                    :
+                                        <span className="badge text-bg-warning">{task.status}</span>
+                                } */}
+                                <span className={`badge ${task.status === 'Completed' ? 'text-bg-success' : 'text-bg-warning'}`}>
+                                    {task.status}
+                                </span>
+                            </td>
                             <td>
                             <button type="button" className="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#editTaskModal">
                                 <svg className="bi"><use xlinkHref="#pencil-square"/></svg>
