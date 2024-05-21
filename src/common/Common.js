2ca52898e6c41e67954bbdce4fb9338ab23b87a5
Azzam Makki <azzam@smartretail.co>
2024-05-21 18:14:04 +0800

    Install react-toastify and use its notifcation on user deleted

---
+++ src/common/Common.js
@@ -0,0 +1,7 @@
+import { toast } from 'react-toastify';
+
+
+// customize by : https://fkhadra.github.io/react-toastify/introduction/
+export const notifySuccess = (msg) => {
+    toast.success(msg);
+}
