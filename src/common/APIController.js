da9a77ec35bfc519e1529c652edb7bad90252181
Azzam Makki <azzam@smartretail.co>
2024-05-21 18:37:34 +0800

    Add common postData function and use it on TaskModel to create a task by api call

--- src/common/APIController.js
+++ src/common/APIController.js
@@ -20,3 +20,25 @@ export async function deleteData(apiURL) {
         console.error(`Error deleting ${apiURL}:`, error);
     }
 }
+
+export async function postData(apiURL, dataObject) {
+    try {
+        const response = await fetch('http://localhost:8000/' + apiURL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(dataObject)
+        });
+
+        if (!response.ok) {
+            console.error(`Error posting task: ${response.statusText}`);
+            return
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error(`Error posting task to ${apiURL}:`, error);
+    }
+}
--- src/components/TaskModal.js
+++ src/components/TaskModal.js
@@ -1,6 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { postData } from '../common/APIController';
+import { notifySuccess } from '../common/Common';
 
 const TaskModal = ({modalId, modalTitle, tags}) => {
+
+    const [taskTitle, setTaskTitle] = useState(null)
+    const [taskDescription, setTaskDescription] = useState(null)
+    const [taskTag, setTaskTag] = useState(null)
+
+    async function handleCreate() {
+
+        const apiURL = 'post/task/';
+
+        const dataObject = {
+            owner_id: 1, //todo : change this
+            title: taskTitle,
+            description: taskDescription,
+            tag_id: 1, //todo : change this
+        }
+
+        await postData(apiURL, dataObject);
+
+        //send notifcation
+        notifySuccess('User was created successfully!')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        handleCreate();
+    }
+
+    console.log(`\n\ntitle: ${taskTitle}\ndescription: ${taskDescription}\ntag: ${taskTag}\n\n`)
 
     return (
         <div className="modal fade" id={modalId} data-backdrop="static" data-keyboard="false" tabIndex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
@@ -14,25 +45,46 @@ const TaskModal = ({modalId, modalTitle, tags}) => {
                     <div className="modal-body">
                         <div className="form-group">
                             <label htmlFor="task-title" className="col-form-label">Title</label>
-                            <input required type="text" className="form-control" id="task-title" name="task-title"/>
+                            <input
+                                required
+                                type="text"
+                                className="form-control"
+                                id="task-title"
+                                name="task-title"
+                                value={taskTitle}
+                                onChange={(e) => setTaskTitle(e.target.value)}
+                            />
                         </div>
                         <div className="form-group">
                             <label htmlFor="task-description" className="col-form-label">Description</label>
-                            <input required type="text" className="form-control" id="task-description" name="task-description"/>
+                            <input
+                                required
+                                type="text"
+                                className="form-control"
+                                id="task-description"
+                                name="task-description"
+                                value={taskDescription}
+                                onChange={(e) => setTaskDescription(e.target.value)}
+                            />
                         </div>
                         <div className="form-group">
                             <label htmlFor="task-tag" className="form-label">Tag</label>
-                            <select className="form-select" id="task-tag" required>
-                            <option value="">Choose...</option>
-                            {tags.map((tag, index ) => (
-                                <option key={index} value={tag}>{tag}</option>
-                            ))}
+                            <select
+                                className="form-select"
+                                id="task-tag"
+                                required
+                                value={taskTag}
+                                onChange={(e) => setTaskTag(e.target.value)}>
+                                <option value="">Choose...</option>
+                                {tags.map((tag, index ) => (
+                                    <option key={index} value={tag}>{tag}</option>
+                                ))}
                             </select>
                         </div>
                     </div>
                     <div className="modal-footer">
                         <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="submit" className="btn btn-primary">Submit</button>
+                        <button type="submit" className="btn btn-primary" onClick={(e) => handleSubmit(e)}>Submit</button>
                     </div>
                     </form>
                 </div>
